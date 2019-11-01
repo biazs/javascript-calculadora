2e4eb143785584e4d202ebcc9ab9ac8979156776
Bia Zoroastro <fabianazs@gmail.com>
2019-11-01 19:07:14 -0300

    Add click event

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,6 +7,7 @@ class CalcController{
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -16,6 +17,16 @@ class CalcController{
             this.setDisplayDateTime();
         }, 1000);
     }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+       
+        buttons.forEach((btn,index) => {
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace("btn-",""));
+        });
+        })
+    }  
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale);
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
