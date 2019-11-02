ebd5039a6d64e31930cb1680068242ecce416823
Bia Zoroastro <fabianazs@gmail.com>
2019-11-01 21:52:16 -0300

    eVal

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -36,12 +36,27 @@ class CalcController{
         return this._operation[this._operation.length-1];
     }
 
-    setLatsOperation(value){
+    setLastOperation(value){
         this._operation[this._operation.length-1] = value;
     }
 
     isOperator(value){
         return (['+', '-', '*','%', '/'].indexOf(value) > -1);
+    }
+    pushOperator(value){
+        this._operation.push(value);
+        if(this._operation.length > 3){
+            this.calc();
+        }
+    }
+    calc(){
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay(){
+        
     }
 
     addOperation(value){
@@ -52,15 +67,20 @@ class CalcController{
                 console.log(value);
             }
             else{
-                this._operation.push(value);
+                this.pushOperator(value);
             }
         }
         else{
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+            if(this.isOperator(value)){
+                this.pushOperator(value);
+            }
+            else{
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+            }
         }
-        
-        console.log(this._operation);
     }
 
     setError(){
